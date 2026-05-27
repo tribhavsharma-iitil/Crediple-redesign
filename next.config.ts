746d63f2372c7d47d44350fd8468a72cbbd788d5
Sunil Verma <104313692+sunil-verma07@users.noreply.github.com>
2026-05-27 23:31:19 +0530

    config change

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   turbopack: {
     root: __dirname,
   },
 
   reactCompiler: true,
+
+  images: {
+    unoptimized: true,
+  },
 
   async headers() {
     return [
