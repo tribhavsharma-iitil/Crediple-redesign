5f48a84118781c36d2059da2f0143a86bcb927cc
tribhav-sharma <tribhav.sharma@iitil.com>
2026-08-14 08:20:20 +0000

    fix: fixed the issue for the related blog post

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -34,7 +34,7 @@ export default async function BlogPostPage({ params }: BlogPageParams) {
         notFound();
     }
 
-    const relatedPosts = getBlogPosts();
+    const relatedPosts = [...getBlogPosts()];
 
     return <BlogPostView post={post} relatedPosts={relatedPosts} />;
 }
